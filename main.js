846f8186afca2493dc2312224b7fe4ceb8d009ab
Arief Badrus Sholeh <arifbadrus08@gmail.com>
2022-09-14 14:51:38 +0700

    Gambar 1 titik

--- main.js
+++ main.js
@@ -3,7 +3,14 @@ function main() {
   var gl = kanvas.getContext("webgl");
 
   // Vertex shader
-  var vertexShaderCode = "void main() {" + "}";
+  var vertexShaderCode = `
+  void main() {
+    float x = 0.0;
+    float y = 0.0;
+    gl_PointSize = 10.0;
+    gl_Position = vec4(x, y, 0.0, 1.0);    
+  }
+  `;
 
   var vertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
   gl.shaderSource(vertexShaderObject, vertexShaderCode);
@@ -11,9 +18,12 @@ function main() {
 
   // Fragment shader
   var fragmentShaderCode = `
+  precision mediump float;
   void main() {
-
-
+    float r = 0.0;
+    float g = 0.0;
+    float b = 1.0;
+    gl_FragColor = vec4(r, g, b, 1.0);
   }
   `;
 
@@ -30,4 +40,6 @@ function main() {
   gl.clearColor(1.0, 0.65, 0.0, 1.0);
   //            red green blue alpha
   gl.clear(gl.COLOR_BUFFER_BIT);
+
+  gl.drawArrays(gl.POINTS, 0, 1);
 }
