1cd634e33e82df5e4c78c6989dff62998639d529
Arief Badrus Sholeh <arifbadrus08@gmail.com>
2022-10-12 14:42:25 +0700

    mengubah grafika interaktif

--- main.js
+++ main.js
@@ -7,26 +7,25 @@ function main() {
     0.0,
     0.0,
     1.0,
-    1.0, // A: kanan atas (CYAN)
+    1.0, // A: kanan atas    (BIRU LANGIT)
     0.0,
     -0.5,
     1.0,
     0.0,
-    1.0, // B: bawah tengah (MAGENTA)
+    1.0, // B: bawah tengah  (MAGENTA)
     -0.5,
     0.0,
     1.0,
     1.0,
-    0.0, // C: kiri atas (KUNING)
+    0.0, // C: kiri atas     (KUNING)
     0.0,
     0.5,
     1.0,
     1.0,
-    1.0, // D: atas tengah (PUTIH)
+    1.0, // D: atas tengah   (PUTIH)
   ];
 
   var buffer = gl.createBuffer();
-
   gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
@@ -35,34 +34,26 @@ function main() {
   attribute vec2 aPosition;
   attribute vec3 aColor;
   uniform float uTheta;
-  uniform vec4 uTranslation;
+  uniform vec2 uDelta;
   varying vec3 vColor;
   void main() {
-    // float x = (-sin(uTheta) * aPosition.x + cos(uTheta) * aPosition.y) + uTranslation.x;
-    // float y = (cos(uTheta) * aPosition.x + sin(uTheta) * aPosition.y) + uTranslation.y;
-    // gl_Position = vec4(x, y, 0.0, 1.0);  
-
-    vec2 position = aPosition;
-
-    mat4 translation = mat4(
-      1.0, 0.0, 0.0, 0.0,
-      0.0, 1.0, 0.0, 0.0, 
-      0.0, 0.0, 1.0, 0.0, 
-      uTranslation.x, uTranslation.y, uTranslation.z, 1.0
-    );
-    mat4 rotation = mat4(
-      cos(uTheta), sin(uTheta), 0.0, 0.0,
-      -sin(uTheta), cos(uTheta), 0.0, 0.0,
-      0.0, 0.0, 1.0, 0.0,
-      0.0, 0.0, 0.0, 1.0
-    );
-
-    gl_Position = translation * rotation * vec4(position, 0.0, 1.0);
-    
-    vColor = aColor;
+      vec2 position = aPosition;
+      mat4 rotation = mat4(
+          cos(uTheta), sin(uTheta), 0.0, 0.0,
+          -sin(uTheta), cos(uTheta), 0.0, 0.0,
+          0.0, 0.0, 1.0, 0.0,
+          0.0, 0.0, 0.0, 1.0
+      );
+      mat4 translation = mat4(
+          1.0, 0.0, 0.0, 0.0,
+          0.0, 1.0, 0.0, 0.0,
+          0.0, 0.0, 1.0, 0.0,
+          uDelta.x, uDelta.y, 0.0, 1.0
+      );
+      gl_Position = translation * rotation * vec4(position, 0.0, 1.0);
+      vColor = aColor;
   }
   `;
-
   var vertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
   gl.shaderSource(vertexShaderObject, vertexShaderCode);
   gl.compileShader(vertexShaderObject); // sampai sini sudah jadi .o
@@ -72,10 +63,9 @@ function main() {
   precision mediump float;
   varying vec3 vColor;
   void main() {
-    gl_FragColor = vec4(vColor, 1.0);
+      gl_FragColor = vec4(vColor, 1.0);
   }
   `;
-
   var fragmentShaderObject = gl.createShader(gl.FRAGMENT_SHADER);
   gl.shaderSource(fragmentShaderObject, fragmentShaderCode);
   gl.compileShader(fragmentShaderObject); // sampai sini sudah jadi .o
@@ -86,19 +76,21 @@ function main() {
   gl.linkProgram(shaderProgram);
   gl.useProgram(shaderProgram);
 
-  // Variabe lokal
+  // Variabel lokal
   var theta = 0.0;
   var freeze = false;
-  var Tx = 0.0,
-    Ty = 0.0;
+  var horizontalSpeed = 0.0;
+  var verticalSpeed = 0.0;
+  var horizontalDelta = 0.0;
+  var verticalDelta = 0.0;
 
   // Variabel pointer ke GLSL
   var uTheta = gl.getUniformLocation(shaderProgram, "uTheta");
-  var translation = gl.getUniformLocation(shaderProgram, "uTranslation");
+  var uDelta = gl.getUniformLocation(shaderProgram, "uDelta");
 
   // Kita mengajari GPU bagaimana caranya mengoleksi
-  // nilai posisi dari ARRAY_BUFFER
-  // untuk setiap verteks yang sedang diproses
+  //  nilai posisi dari ARRAY_BUFFER
+  //  untuk setiap verteks yang sedang diproses
   var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
   gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 5 * Float32Array.BYTES_PER_ELEMENT, 0);
   gl.enableVertexAttribArray(aPosition);
@@ -113,45 +105,46 @@ function main() {
   }
   document.addEventListener("click", onMouseClick);
   // Papan ketuk
-  function onKeyDown(event) {
-    if (event.keyCode == 32) freeze = !freeze;
-  }
-  function onKeyUp(event) {
-    if (event.keyCode == 32) freeze = !freeze;
-  }
-  function moveSquare(event) {
-    if (event.keyCode == 87 && event.keyCode == 68) {
-      Ty += 0.1;
-      Tx += 0.1;
-    } else if (event.keyCode == 83) {
-      Ty -= 0.1;
-    } else if (event.keyCode == 65) {
-      Tx -= 0.1;
+  function onKeydown(event) {
+    if (event.keyCode == 32) freeze = !freeze; // spasi
+    // Gerakan horizontal: a ke kiri, d ke kanan
+    if (event.keyCode == 65) {
+      // a
+      horizontalSpeed = -0.01;
     } else if (event.keyCode == 68) {
-      Tx += 0.1;
-    } else if (event.keyCode == 87) {
-      Ty += 0.1;
+      // d
+      horizontalSpeed = 0.01;
     }
-    gl.uniform4f(translation, Tx, Ty, 0.0, 1.0);
+    // Gerakan vertikal: w ke atas, s ke bawah
+    if (event.keyCode == 87) {
+      // w
+      verticalSpeed = -0.01;
+    } else if (event.keyCode == 83) {
+      // s
+      verticalSpeed = 0.01;
+    }
   }
-  document.addEventListener("keydown", onKeyDown);
-  // document.addEventListener("keyup", onKeyUp);
-  document.addEventListener("keydown", moveSquare);
+  function onKeyup(event) {
+    if (event.keyCode == 32) freeze = !freeze;
+    if (event.keyCode == 65 || event.keyCode == 68) horizontalSpeed = 0.0;
+    if (event.keyCode == 87 || event.keyCode == 83) verticalSpeed = 0.0;
+  }
+  document.addEventListener("keydown", onKeydown);
+  document.addEventListener("keyup", onKeyup);
 
   function render() {
-    gl.clearColor(1.0, 0.65, 0.0, 1.0);
-    //            red green blue alpha
+    gl.clearColor(1.0, 0.65, 0.0, 1.0); // Oranye
+    //            Merah     Hijau   Biru    Transparansi
     gl.clear(gl.COLOR_BUFFER_BIT);
     if (!freeze) {
       theta += 0.1;
       gl.uniform1f(uTheta, theta);
     }
+    horizontalDelta += horizontalSpeed;
+    verticalDelta -= verticalSpeed;
+    gl.uniform2f(uDelta, horizontalDelta, verticalDelta);
     gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
-    // POINTS
-    // LINES, LINE_LOOP, LINE_STRIP
-    // TRIANGLES, TRIANGLE_STRIP, TRIANGLE_FAN
     requestAnimationFrame(render);
   }
-
   requestAnimationFrame(render);
 }
