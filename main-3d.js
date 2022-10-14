c0fdd09b01923343fb87697f2e850180996fb72a
Arief Badrus Sholeh <arifbadrus08@gmail.com>
2022-10-14 14:48:26 +0700

    Muncul kubus

--- main-3d.js
+++ main-3d.js
@@ -4,25 +4,66 @@ function main() {
 
   // prettier-ignore
   var vertices = [
-    0.5, 0.0, 0.0, 1.0, 1.0,    // A: kanan atas    (BIRU LANGIT)
-    0.0, -0.5, 1.0, 0.0, 1.0,   // B: bawah tengah  (MAGENTA)
-    -0.5, 0.0, 1.0, 1.0, 0.0,   // C: kiri atas     (KUNING)
-    0.0, 0.5, 1.0, 1.0, 1.0,    // D: atas tengah   (PUTIH)
+    // Face A       // Red
+    -1, -1, -1,     1, 0, 0,    // Index:  0    
+     1, -1, -1,     1, 0, 0,    // Index:  1
+     1,  1, -1,     1, 0, 0,    // Index:  2
+    -1,  1, -1,     1, 0, 0,    // Index:  3
+    // Face B       // Yellow
+    -1, -1,  1,     1, 1, 0,    // Index:  4
+     1, -1,  1,     1, 1, 0,    // Index:  5
+     1,  1,  1,     1, 1, 0,    // Index:  6
+    -1,  1,  1,     1, 1, 0,    // Index:  7
+    // Face C       // Green
+    -1, -1, -1,     0, 1, 0,    // Index:  8
+    -1,  1, -1,     0, 1, 0,    // Index:  9
+    -1,  1,  1,     0, 1, 0,    // Index: 10
+    -1, -1,  1,     0, 1, 0,    // Index: 11
+    // Face D       // Blue
+     1, -1, -1,     0, 0, 1,    // Index: 12
+     1,  1, -1,     0, 0, 1,    // Index: 13
+     1,  1,  1,     0, 0, 1,    // Index: 14
+     1, -1,  1,     0, 0, 1,    // Index: 15
+    // Face E       // Orange
+    -1, -1, -1,     1, 0.5, 0,  // Index: 16
+    -1, -1,  1,     1, 0.5, 0,  // Index: 17
+     1, -1,  1,     1, 0.5, 0,  // Index: 18
+     1, -1, -1,     1, 0.5, 0,  // Index: 19
+    // Face F       // White
+    -1,  1, -1,     1, 1, 1,    // Index: 20
+    -1,  1,  1,     1, 1, 1,    // Index: 21
+     1,  1,  1,     1, 1, 1,    // Index: 22
+     1,  1, -1,     1, 1, 1     // Index: 23
+  ];
+
+  // prettier-ignore
+  var indices = [
+    0, 1, 2,     0, 2, 3,     // Face A
+    4, 5, 6,     4, 6, 7,     // Face B
+    8, 9, 10,    8, 10, 11,   // Face C
+    12, 13, 14,  12, 14, 15,  // Face D
+    16, 17, 18,  16, 18, 19,  // Face E
+    20, 21, 22,  20, 22, 23   // Face F     
   ];
 
   var buffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
+  var indexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+
   // Vertex shader
   var vertexShaderCode = `
-    attribute vec2 aPosition;
+    attribute vec3 aPosition;
     attribute vec3 aColor;
     uniform mat4 uModel;
+    uniform mat4 uView;
+    uniform mat4 uProjection;
     varying vec3 vColor;
     void main() {
-        vec2 position = aPosition;
-        gl_Position = uModel * vec4(position, 0.0, 1.0);
+        gl_Position = uProjection * uView * uModel * vec4(aPosition, 1.0);
         vColor = aColor;
     }
     `;
@@ -58,21 +99,36 @@ function main() {
 
   // Variabel pointer ke GLSL
   var uModel = gl.getUniformLocation(shaderProgram, "uModel");
+  // View
+  var cameraX = 0.0;
+  var cameraZ = 5.0;
+  var uView = gl.getUniformLocation(shaderProgram, "uView");
+  var view = glMatrix.mat4.create();
+  glMatrix.mat4.lookAt(
+    view,
+    [cameraX, 0.0, cameraZ], // the location of the eye or the camera
+    [cameraX, 0.0, -10], // the point where the camera look at
+    [0.0, 1.0, 0.0]
+  );
+  // Projection
+  var uProjection = gl.getUniformLocation(shaderProgram, "uProjection");
+  var perspective = glMatrix.mat4.create();
+  glMatrix.mat4.perspective(perspective, Math.PI / 3, 1.0, 0.5, 10.0);
 
   // Kita mengajari GPU bagaimana caranya mengoleksi
   //  nilai posisi dari ARRAY_BUFFER
   //  untuk setiap verteks yang sedang diproses
   var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
   // prettier-ignore
-  gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 
-    5 * Float32Array.BYTES_PER_ELEMENT, 
+  gl.vertexAttribPointer(aPosition, 3, gl.FLOAT, false, 
+    6 * Float32Array.BYTES_PER_ELEMENT, 
     0);
   gl.enableVertexAttribArray(aPosition);
   var aColor = gl.getAttribLocation(shaderProgram, "aColor");
   //   prettier-ignore
   gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, 
-    5 * Float32Array.BYTES_PER_ELEMENT, 
-    2 * Float32Array.BYTES_PER_ELEMENT);
+    6 * Float32Array.BYTES_PER_ELEMENT, 
+    3 * Float32Array.BYTES_PER_ELEMENT);
   gl.enableVertexAttribArray(aColor);
 
   // Grafika interaktif
@@ -120,15 +176,15 @@ function main() {
     verticalDelta -= verticalSpeed;
 
     var model = glMatrix.mat4.create(); // Membuat matriks identitas
-    // prettier-ignore
-    glMatrix.mat4.translate(
-        model, model, [horizontalDelta, verticalDelta, 0.0]);
-    // prettier-ignore
-    glMatrix.mat4.rotateZ(
-        model, model, theta);
+    glMatrix.mat4.translate(model, model, [horizontalDelta, verticalDelta, 0.0]);
+    glMatrix.mat4.rotateX(model, model, theta);
+    glMatrix.mat4.rotateY(model, model, theta);
+    glMatrix.mat4.rotateZ(model, model, theta);
     gl.uniformMatrix4fv(uModel, false, model);
+    gl.uniformMatrix4fv(uView, false, view);
+    gl.uniformMatrix4fv(uProjection, false, perspective);
 
-    gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+    gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
     requestAnimationFrame(render);
   }
   requestAnimationFrame(render);
